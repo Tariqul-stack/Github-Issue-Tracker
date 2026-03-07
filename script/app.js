009af0b1c3142ce1c090fbf531a54a742c2b419e
Tariqul911 <zxtariqul911@gmail.com>
2026-03-07 14:37:06 +0600

    fixed html link issue

--- script/app.js
+++ script/app.js
@@ -9,7 +9,7 @@ document.getElementById('sign-btn').addEventListener('click', function(){
     if(userName === "admin" && userPassword === "admin123"){
         alert("Sign In Success");
 
-        window.location.assign('/home.html');
+        window.location.assign('home.html');
 
     }
     else{
