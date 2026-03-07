f0283ecfdb86a7882d3f24ae121114eb882c20ad
Tariqul911 <zxtariqul911@gmail.com>
2026-03-07 12:22:35 +0600

    added sign in page functionality

--- script/app.js
+++ script/app.js
@@ -1,1 +1,20 @@
-console.log("connected");
+// Login Page 
+document.getElementById('sign-btn').addEventListener('click', function(){
+    const userNameInput = document.getElementById('usernameInput');
+    const userName = userNameInput.value;
+
+    const userPasswordInput = document.getElementById('passwordInput');
+    const userPassword = userPasswordInput.value;
+
+    if(userName === "admin" && userPassword === "admin123"){
+        alert("Sign In Success");
+
+        window.location.assign('/home.html');
+
+    }
+    else{
+        alert("Sign In Failed, USE Demo Credentials");
+        return;
+    }
+
+})
